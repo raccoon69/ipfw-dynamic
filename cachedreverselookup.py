f1f872e900bc8a2a37fec56c6b196b22e0335206
Jim Housley <raccoon69@users.noreply.github.com>
2024-04-07 17:59:52 -0400

    Use a thread to do the revese lookup.
    Using a thread will allow lookups to be done in the background.  It will return the raw IP at first but once the lookup succeeds it will return host names.

--- cachedreverselookup.py
+++ cachedreverselookup.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 
 class CachedReverseLookup:
@@ -15,18 +16,29 @@ class CachedReverseLookup:
         self.resolver_count = 0
 
     #
+    # Do the actual DNS reverse lookup
+    def get_hostname(self, ip_address: str) -> None:
+        try:
+            host = socket.gethostbyaddr(ip_address)
+            self.cache[ip_address] = host[0]
+        except:
+            pass
+
+    #
     # convert an IP address to a host by either using the cache or
     # doing a lookup and adding it to the cache
     def lookup(self, ip_address: str) -> str:
         if ip_address in self.cache:
             return self.cache[ip_address]
+
         if self.resolver_count < 10:
             self.resolver_count += 1
-            try:
-                host = socket.gethostbyaddr(ip_address)
-                self.cache[ip_address] = host[0]
-            except:
-                self.cache[ip_address] = ip_address
-            return self.cache[ip_address]
-        else:
-            return ip_address
+
+            # Set the the result value to itself, this will prevent more lookups
+            self.cache[ip_address] = ip_address
+
+            # Start a thread for each lookup
+            x = threading.Thread(target=self.get_hostname, args=(ip_address,))
+            x.start()
+
+        return ip_address
